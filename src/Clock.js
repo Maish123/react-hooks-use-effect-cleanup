3ed399c23e7277a45bdc7434b1f3708f539b790c
Denis Maina <Denis.maina@student.moringaschool.com>
2022-07-19 14:31:15 +0300

    initial commit

--- src/Clock.js
+++ src/Clock.js
@@ -4,10 +4,16 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    console.log("hi")
+    const timerID = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    return function cleanup(){
+      clearInterval(timerID)
+    }
   }, []);
+  console.log("bye")
 
   return <div>{time.toString()}</div>;
 }
